ae9a37aaac7944064c058a893af3d6cdf54ad7a3
Alex <alex.h.sato@gmail.com>
2017-03-11 23:27:33 +1100

    change for lauchlans testing

--- src/game/main.java
+++ src/game/main.java
@@ -4,6 +4,10 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int i = 1;
+		while (i==1) {
+			System.out.println("eat shit");
+		}
 		//this is a test
 	}
 
